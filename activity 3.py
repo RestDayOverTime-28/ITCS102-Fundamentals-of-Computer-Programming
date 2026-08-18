bf85f1ea8de1c69e659b881512bd43153953c65a
RestDayOverTime-28 <restdayovertime28@gmail.com>
2026-08-18 20:08:41 +0800

    Add files via upload

---
+++ activity 3.py
@@ -0,0 +1,48 @@
+# STRINGS
+first_name = "Aizen"
+middle_name = "Palenzuela"
+last_name = "De Guzman"
+favorite_color1, favorite_color2 = "Blue", "Purple"
+
+print(
+    "My name is " + first_name + " " + middle_name + " " + last_name + ".",
+    "My favorite colors are " + favorite_color1 + " and " + favorite_color2 + "."
+)
+
+# INTEGERS
+age = 18
+phone_number = 991
+id_number = 26
+favorite_number = 1290
+
+print(
+    "My age is " + str(age) + ".",
+    "My phone number is " + str(phone_number) + ".",
+    "My ID number is " + str(id_number) + ".",
+    "My favorite number is " + str(favorite_number) + "."
+)
+
+# FLOATS
+height = 5.9
+weight = 46.0
+b_temperature = 36.5
+a_temperature = 37.5
+
+print(
+    "My height is " + str(height) + ".",
+    "My weight is " + str(weight) + ".",
+    "My body temperature is " + str(a_temperature) + " while my average temperature is " + str(b_temperature) + ".",
+)
+
+# BOOLEANS
+isStudent = True
+isEmployed = False
+isWell = True
+isRich = False
+
+print(
+    "Am I a student? " + str(isStudent) + ".",
+    "Am I employed? " + str(isEmployed) + ".",
+    "Am I well? " + str(isWell) + ".",
+    "Am I rich? " + str(isRich) + "."
+)
