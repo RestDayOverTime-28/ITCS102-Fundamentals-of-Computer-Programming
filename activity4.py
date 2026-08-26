d7bd1be7b3f7a0daa30dde2ac6dc3244fa6dbbef
RestDayOverTime-28 <restdayovertime28@gmail.com>
2026-08-26 21:03:19 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,8 @@
+# \n
+# \t
+# \r
+# \b
+# \\
+# \"
+
+print("Fly, broken wings \nI know you are still with \n\tme \nAll I need is a \n\t\tnudge to \n\t\t\tget me \n\t\t\t\tstarted \n\t\t\t\tFly, broken wings \n\t\tTo somewhere we can be free \nCloser to our \"ideal\"")
---
+++ activity5.py
@@ -0,0 +1,20 @@
+# OPERATIONS
+
+a = 6
+b = 7
+
+sum = a + b
+diff = a - b
+product = a * b
+division = a / b
+floorDivision = a // b
+exponential = a ** b
+modulus = a % b
+
+print("The sum of", a, "and", b, "is:", sum)
+print("The difference of", a, "and", b, "is:", diff)
+print("The product of", a, "and", b, "is:", product)
+print("The quotient of", a, "and", b, "is:", division)
+print("The remainder of", a, "and", b, "is:", modulus)
+print("The result of", a, "raised to the power of", b, "is:", exponential)
+print("The floor division of", a, "and", b, "is:", floorDivision)
